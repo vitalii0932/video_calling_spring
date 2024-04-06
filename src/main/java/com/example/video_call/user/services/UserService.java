95c0f126d03b0e4d1f07fa6d942118042222eda8
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-06 18:37:06 +0300

    user class and user serveci with register and login methods

---
+++ src/main/java/com/example/video_call/user/services/UserService.java
@@ -0,0 +1,34 @@
+package com.example.video_call.user.services;
+
+import com.example.video_call.user.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Service
+public class UserService {
+    private static final List<User> USERS_LIST = new ArrayList<>();
+
+    public void register(User user) {
+        if(USERS_LIST.stream().anyMatch(someUser -> someUser.getEmail().equals(user.getEmail()))) {
+            throw new RuntimeException("The user with this email is registered in the system");
+        }
+        user.setStatus("online");
+        USERS_LIST.add(user);
+    }
+
+    public User login(User user) {
+        var userId = IntStream.range(0, USERS_LIST.size())
+                .filter(i -> USERS_LIST.get(i).getEmail().equals(user.getEmail()))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        var connectedUser = USERS_LIST.get(userId);
+        if (!connectedUser.getPassword().equals(user.getPassword())) {
+            throw new RuntimeException("Password incorrect");
+        }
+        user.setStatus("online");
+        return connectedUser;
+    }
+}
