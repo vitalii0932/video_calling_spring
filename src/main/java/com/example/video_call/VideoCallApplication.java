7baea719bcd2442319c38ddcf7ef46a25909b2d4
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-04-06 18:12:57 +0300

    initial commit

---
+++ src/main/java/com/example/video_call/VideoCallApplication.java
@@ -0,0 +1,13 @@
+package com.example.video_call;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/video_call/VideoCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.video_call;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
